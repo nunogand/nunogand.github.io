807b70657512f28c6a510dd2f0fed0bb773e4f36
Nuno Rodrigues <nunogand@gmail.com>
2020-07-21 01:45:40 +0100

    Update sistema_solar.js

--- custom/js/sistema_solar.js
+++ custom/js/sistema_solar.js
@@ -1,4 +1,8 @@
+<script>
   window.console = window.console || function(t) {};
+</script>
+<script>
   if (document.location.search.match(/type=embed/gi)) {
     window.parent.postMessage("resize", "*");
   }
+</script>
