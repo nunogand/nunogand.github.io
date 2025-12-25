67ddb4accdcf735c528c5739d966366ce8979a98
nunogand <nunogand@gmail.com>
2025-12-25 15:50:56 +0000

    aria labels

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ $(document).ready(function () {
 
   SimpleJekyllSearch({
     searchInput: document.getElementById('js-search-input'),
-    resultsContainer: document.getElementById('js-results-container'),
+    resultsContainer: document.getElementById('resultados'),
     json: '/search.json',
     searchResultTemplate: '<li><a href="{url}">{title}</a></li>',
     noResultsText: '<li>No results found</li>'
